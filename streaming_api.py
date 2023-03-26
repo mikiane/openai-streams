d410effd9808985abeb730a81412a655dfa04520
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2023-03-26 12:05:56 +0200

    update Ports

--- streaming_api.py
+++ streaming_api.py
@@ -18,8 +18,8 @@ def stream_chat():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    app.run(threaded=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-    #app.run(threaded=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
