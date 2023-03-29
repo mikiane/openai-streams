f3bed8740821f5e8edb6356a6b519b433009a128
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2023-03-29 13:41:02 +0200

    Update Google cloud

---
+++ inbubble-json.js
@@ -0,0 +1,73 @@
+async function openaireq(consigne, texte) {
+    const data = {
+      consigne: encodeURIComponent(consigne),
+      texte: encodeURIComponent(texte),
+    };
+  
+    const response = await fetch("https://backend.brightness-agency.com/stream_chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  
+    const reader = response.body.getReader();
+    const textDecoder = new TextDecoder("utf-8");
+          
+    while (true) {
+        const { value, done } = await reader.read();
+        if (done) break;
+        const chunk = textDecoder.decode(value);
+        responseDiv.innerHTML += chunk;
+        console.log(chunk);
+    }
+}
+  
+    
+var consigne = "développer ce texte";
+var texte = "ceci est un texte à développer";
+        
+    
+openaireq(consigne, texte);
+  
+
+
+  
+  
+async function openaireq(consigne, texte) {
+    console.log("infunc");
+    const formData = new FormData();
+    const encodedConsigne = escapeSpecialChars(consigne);
+    const encodedTexte = escapeSpecialChars(texte);
+    console.log("Consigne:", encodedConsigne);
+    console.log("Texte:", encodedTexte);
+    formData.append("consigne", encodedConsigne);
+    formData.append("texte", encodedTexte);
+  
+    const response = await fetch("https://backend.brightness-agency.com/stream_chat", {
+      method: "POST",
+      body: formData,
+    });
+  
+    const reader = response.body.getReader();
+    const textDecoder = new TextDecoder("utf-8");
+          
+  while (true) {
+      const { value, done } = await reader.read();
+      if (done) break;
+      const chunk = textDecoder.decode(value);
+      responseDiv.innerHTML += chunk;
+     console.log(chunk);
+  }
+  }
+  
+    
+    var consigne = "développer ce texte";
+    var texte = "ceci est un texte à développer";
+    
+    console.log("Helo");
+    
+    
+    openaireq(consigne, texte);
+  
